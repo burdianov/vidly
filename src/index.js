2a8296372533fd26babcc4af448a82aec05b7af6
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-01-18 17:38:46 +0400

    add movies homework component, not fully functionable

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/react-fontawesome";
+import Movies from "./components/movies_homework.jsx";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Movies />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
