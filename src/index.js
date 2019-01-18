29ef7136c9cdbba8bfe6227454bdc2a76453f7f4
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-01-18 18:14:30 +0400

    build the Movies component

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/react-fontawesome";
 import Movies from "./components/movies_homework.jsx";
 
-ReactDOM.render(<Movies />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
